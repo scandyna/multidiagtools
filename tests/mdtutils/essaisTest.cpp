28fcb6a1b6f795f3ffaa9c9120f5af8e87ec9923
Philippe Steinmann <philippe.steinmann@gmail.com>
2011-10-18 09:35:12 +0200

    Intermediate commit after some trying with CMake

--- tests/mdtutils/essaisTest.cpp
+++ tests/mdtutils/essaisTest.cpp
@@ -1,12 +1,13 @@
 extern void fonctionEssais();
 
 #include <QApplication>
+#include "mdtTest.h"
 
 int main(int argc, char **argv)
 {
   QApplication app(argc, argv);
 
-  
+  mdtTest t;
 
   return app.exec();
 }
