33f9b107e2bdf02c8c686215cf8d758daa050fbd
Philippe Steinmann <philippe.steinmann@gmail.com>
2017-09-18 12:16:34 +0200

    Try to fix compile error with Clang on Travis CI

--- cmake/caches/DebugClang.cmake
+++ cmake/caches/DebugClang.cmake
@@ -2,7 +2,6 @@
 set(CMAKE_C_COMPILER clang CACHE STRING "C compiler")
 set(CMAKE_CXX_COMPILER clang++ CACHE STRING "C++ compiler")
 set(CMAKE_BUILD_TYPE Debug CACHE STRING "Build type")
-
 set(MDT_CXX_FLAGS_TO_APPEND "-O3 -Wall -Wextra" CACHE STRING "")
 
 # On *NIX we use mult-arch install (using GNUInstallDirs)
