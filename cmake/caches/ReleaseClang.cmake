99f0923fac18cad036c7e2af6f34631d60113497
Philippe Steinmann <philippe.steinmann@gmail.com>
2017-09-18 10:06:38 +0200

    Added cache files for Clang

---
+++ cmake/caches/ReleaseClang.cmake
@@ -0,0 +1,12 @@
+
+set(CMAKE_C_COMPILER clang CACHE STRING "C compiler")
+set(CMAKE_CXX_COMPILER clang++ CACHE STRING "C++ compiler")
+set(CMAKE_BUILD_TYPE Release CACHE STRING "Build type") 
+set(MDT_CXX_FLAGS_TO_APPEND "-O3 -Wall -Wextra -Werror -flto" CACHE STRING "")
+
+# On *NIX we use mult-arch install (using GNUInstallDirs)
+# This only works if CMAKE_INSTALL_PREFIX is set to /usr
+# Because default is to build installations package, default this to /usr
+if(UNIX)
+  set(CMAKE_INSTALL_PREFIX /usr CACHE FILE "Installation prefix")
+endif()
