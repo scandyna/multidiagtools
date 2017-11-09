9939b6a152d62f55084bd5d98f387f1c0b1413d7
Philippe Steinmann <philippe.steinmann@gmail.com>
2017-11-09 13:03:39 +0100

    Implemented QmFileName for translations

---
+++ libs/DeployUtils_Core/tests/src/QmFileNameBenchmark.h
@@ -0,0 +1,39 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2011-2017 Philippe Steinmann.
+ **
+ ** This file is part of multiDiagTools library.
+ **
+ ** multiDiagTools is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** multiDiagTools is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with multiDiagTools.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+#ifndef QM_FILE_NAME_BEBCHMARK_H
+#define QM_FILE_NAME_BEBCHMARK_H
+
+#include "TestBase.h"
+
+class QmFileNameBenchmark : public TestBase
+{
+ Q_OBJECT
+
+ private slots:
+
+  void initTestCase();
+  void cleanupTestCase();
+
+  void contructFromFullNameBenchmark();
+  void contructFromFullNameBenchmark_data();
+};
+
+#endif // #ifndef QM_FILE_NAME_BEBCHMARK_H
