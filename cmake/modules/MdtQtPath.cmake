536dfdbb0fb668823c96a3021411999dec7918a5
Philippe Steinmann <philippe.steinmann@gmail.com>
2017-12-03 13:46:44 +0100

    Add MdtQtPath CMake module

---
+++ cmake/modules/MdtQtPath.cmake
@@ -0,0 +1,40 @@
+#.rst:
+# MdtQtPath
+# --------------
+#
+# Get the prefix of the Qt library on which a Mdt installation depends.
+#
+# Result Variables
+# ^^^^^^^^^^^^^^^^
+#
+# Inclusion of this module defines the following variables:
+#
+# ``QT_PREFIX_PATH``
+#
+#   Path to the Qt library on which a Mdt installation depends.
+#
+
+# list(LENGTH CMAKE_PREFIX_PATH _cmake_prefix_path_length)
+# if(${_cmake_prefix_path_length} LESS 1)
+#   message(FATAL_ERROR "")
+# endif()
+
+find_file(
+  _qt_prefix_path_file
+  NAMES "QtPrefixPath.txt"
+  PATHS ${CMAKE_PREFIX_PATH}
+  NO_DEFAULT_PATH
+)
+
+if(NOT EXISTS "${_qt_prefix_path_file}")
+  message(FATAL_ERROR "Could not locate QtPrefixPath.txt. Searched in: ${CMAKE_PREFIX_PATH}")
+endif()
+
+file(STRINGS "${_qt_prefix_path_file}" QT_PREFIX_PATH)
+
+# Check that QT_PREFIX_PATH points to a Qt installation
+if(NOT QT_PREFIX_PATH)
+  message(FATAL_ERROR "QT_PREFIX_PATH was not set, please check that CMAKE_PREFIX_PATH contains a path to a Mdt installation.")
+endif()
+
+message("QT_PREFIX_PATH: ${QT_PREFIX_PATH}")
