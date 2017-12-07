be48d122baca062c06738ac2d4587051cf7d1933
Philippe Steinmann <philippe.steinmann@gmail.com>
2017-12-07 10:59:52 +0100

    Create example for Translation::LanguageSelection

---
+++ libs/Translation_Widgets/examples/MultiLanguageApplication/translations/multiLanguageApplication_de.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>MultiLanguageMainWindow</name>
+    <message>
+        <location filename="../src/MultiLanguageMainWindow.ui" line="14"/>
+        <source>Multi language application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MultiLanguageMainWindow.ui" line="38"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MultiLanguageMainWindow.ui" line="44"/>
+        <source>&amp;Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MultiLanguageMainWindow.ui" line="53"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MultiLanguageMainWindow.ui" line="67"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MultiLanguageMainWindow.ui" line="21"/>
+        <source>This is maybe translated text.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ libs/Translation_Widgets/examples/MultiLanguageApplication/translations/multiLanguageApplication_fr.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>MultiLanguageMainWindow</name>
+    <message>
+        <location filename="../src/MultiLanguageMainWindow.ui" line="14"/>
+        <source>Multi language application</source>
+        <translation>Application multi-langues</translation>
+    </message>
+    <message>
+        <source>Example text</source>
+        <translation type="vanished">Texte d&apos;example</translation>
+    </message>
+    <message>
+        <location filename="../src/MultiLanguageMainWindow.ui" line="38"/>
+        <source>&amp;File</source>
+        <translation>&amp;Fichier</translation>
+    </message>
+    <message>
+        <location filename="../src/MultiLanguageMainWindow.ui" line="44"/>
+        <source>&amp;Language</source>
+        <translation>&amp;Langue</translation>
+    </message>
+    <message>
+        <location filename="../src/MultiLanguageMainWindow.ui" line="53"/>
+        <source>toolBar</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/MultiLanguageMainWindow.ui" line="67"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Quitter</translation>
+    </message>
+    <message>
+        <location filename="../src/MultiLanguageMainWindow.ui" line="21"/>
+        <source>This is maybe translated text.</source>
+        <translation>Ceci est un texte potentiellement traduit.</translation>
+    </message>
+</context>
+</TS>
