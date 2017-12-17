cb58e881bf9a027579c08d469bb6cebd8972278b
Philippe Steinmann <philippe.steinmann@gmail.com>
2017-12-17 11:31:15 +0100

    Create Entity Sql

---
+++ libs/Entity_Sql/examples/ClientEntitySqlExample/src/main.cpp
@@ -0,0 +1,36 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2011-2017 Philippe Steinmann.
+ **
+ ** This file is part of Mdt library.
+ **
+ ** Mdt is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** Mdt is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with Mdt.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+// #include "MultiLanguageMainWindow.h"
+#include <QCoreApplication>
+
+int main(/*int argc, char **argv*/)
+{
+//   QCoreApplication app(argc, argv);
+
+//   DataStruct ds;
+//   qDebug() << "ds, " << ds.id.columnName() << ": " << ds.id << " , name: " << ds.name.columnName() << ": " << ds.name;
+// 
+//   DataStruct2 ds2;
+//   qDebug() << "ds2, " << ds2.id.columnName() << ": " << ds2.id << " , name: " << ds2.name.columnName() << ": " << ds2.name;
+
+  return 0;
+//   return app.exec();
+}
