50ceb753228fdb713552b659a902109aa1e96338
Philippe Steinmann <philippe.steinmann@gmail.com>
2018-02-22 03:26:59 +0100

    Entity: implement DataList

---
+++ libs/Entity_Core/src/Mdt/Entity/DataList.h
@@ -0,0 +1,233 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2011-2018 Philippe Steinmann.
+ **
+ ** This file is part of Mdt library.
+ **
+ ** Mdt is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** Mdt is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with Mdt.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+#ifndef MDT_ENTITY_DATA_LIST_H
+#define MDT_ENTITY_DATA_LIST_H
+
+#include "DataTemplate.h"
+#include "Mdt/Container/StlContainer.h"
+#include <QtGlobal>
+#include <iterator>
+#include <vector>
+
+namespace Mdt{ namespace Entity{
+
+  /*! \brief Class template to represent a list of entity data
+   *
+   * Example:
+   * \code
+   * class ArticleData : public DataTemplate<ArticleDataStruct, ArticleDef>
+   * {
+   * };
+   *
+   * using ArticleDataList = DataList<ArticleData>;
+   * \endcode
+   */
+  template<typename EntityData, typename Container = std::vector<EntityData> >
+  class DataList
+  {
+   public:
+
+    /*! \brief STL-style const iterator
+     */
+    using const_iterator = typename Container::const_iterator;
+
+    /*! \brief STL-style iterator
+     */
+    using iterator = typename Container::iterator;
+
+    /*! \brief STL-style const pointer
+     */
+    using const_pointer = typename Container::const_pointer;
+
+    /*! \brief STL-style pointer
+     */
+    using pointer = typename Container::pointer;
+
+    /*! \brief STL-style const reference
+     */
+    using const_reference = typename Container::const_reference;
+
+    /*! \brief STL-style reference
+     */
+    using reference = typename Container::reference;
+
+    /*! \brief STL-style size
+     */
+    using size_type = typename Container::size_type;
+
+    /*! \brief STL-syle difference
+     */
+    using difference_type = typename Container::difference_type;
+
+    /*! \brief STL-style value
+     */
+    using value_type = typename Container::value_type;
+
+    /*! \brief Construct a empty data list
+     */
+    explicit DataList() = default;
+
+    /*! \brief Construct a data list with a initial size of \a size default constructed elements
+     */
+    explicit DataList(int size)
+     : mContainer( Mdt::Container::initializeContainer<Container>(size, value_type{}) )
+    {
+    }
+
+    /*! \brief Copy construct a data list from \a other
+     */
+    DataList(const DataList & other) = default;
+
+    /*! \brief Copy assign \a other to this data list
+     */
+    DataList & operator=(const DataList & other) = default;
+
+    /*! \brief Move construct a data list from \a other
+     */
+    DataList(DataList && other) = default;
+
+    /*! \brief Move assign \a other to this data list
+     */
+    DataList & operator=(DataList && other) = default;
+
+    /*! \brief Check if this data list is empty
+     */
+    bool isEmpty() const
+    {
+      return Mdt::Container::containerIsEmpty(mContainer);
+    }
+
+    /*! \brief Get count of element in this data list
+     *
+     * \sa size()
+     */
+    int count() const
+    {
+      return size();
+    }
+
+    /*! \brief Get the size of this list
+     *
+     * \sa count()
+     */
+    int size() const
+    {
+      return Mdt::Container::containerSize(mContainer);
+    }
+
+    /*! \brief Access element at \a index
+     *
+     * \pre \a index must be in valid range ( 0 <= \a index < size() ).
+     * \sa operator[]
+     */
+    const EntityData & at(int index) const
+    {
+      Q_ASSERT(index >= 0);
+      Q_ASSERT(index < size());
+      return *Mdt::Container::constIteratorAtIndex(mContainer, index);
+    }
+
+    /*! \brief Access element at \a index as modifiable reference
+     *
+     * \pre \a index must be in valid range ( 0 <= \a index < size() ).
+     * \sa at()
+     */
+    EntityData & operator[](int index)
+    {
+      Q_ASSERT(index >= 0);
+      Q_ASSERT(index < size());
+      return *Mdt::Container::iteratorAtIndex(mContainer, index);
+    }
+
+    /*! \brief Add \a data to the end of this list
+     */
+    void append(const EntityData & data)
+    {
+      Mdt::Container::appendToContainer(mContainer, data);
+    }
+
+    /*! \brief Remove last element from this data list
+     *
+     * \pre this data list must not be empty
+     */
+    void removeLast()
+    {
+      Q_ASSERT(!isEmpty());
+      Mdt::Container::removeLastFromContainer(mContainer);
+    }
+
+    /*! \brief Clear this data list
+     */
+    void clear()
+    {
+      mContainer.clear();
+    }
+
+    /*! \brief Get a STL-style iterator pointing to the first element in this data list
+     */
+    iterator begin()
+    {
+      return std::begin(mContainer);
+    }
+
+    /*! \brief Get a STL-style iterator pointing past the last element in this data list
+     */
+    iterator end()
+    {
+      return std::end(mContainer);
+    }
+
+    /*! \brief Get a STL-style const iterator pointing to the first element in this data list
+     */
+    const_iterator cbegin() const
+    {
+      return std::cbegin(mContainer);
+    }
+
+    /*! \brief Get a STL-style const iterator pointing past the last element in this data list
+     */
+    const_iterator cend() const
+    {
+      return std::cend(mContainer);
+    }
+
+    /*! \brief Get a STL-style const iterator pointing to the first element in this data list
+     */
+    const_iterator begin() const
+    {
+      return cbegin();
+    }
+
+    /*! \brief Get a STL-style const iterator pointing past the last element in this data list
+     */
+    const_iterator end() const
+    {
+      return cend();
+    }
+
+   private:
+
+    Container mContainer;
+  };
+
+}} // namespace Mdt{ namespace Entity{
+
+#endif // #ifndef MDT_ENTITY_DATA_LIST_H
---
+++ libs/Entity_Core/tests/src/DataListTest.cpp
@@ -0,0 +1,125 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2011-2018 Philippe Steinmann.
+ **
+ ** This file is part of multiDiagTools library.
+ **
+ ** multiDiagTools is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** multiDiagTools is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with multiDiagTools.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+#include "DataListTest.h"
+#include "Mdt/Entity/Def.h"
+#include "Mdt/Entity/DataList.h"
+#include <QVector>
+#include <QList>
+#include <vector>
+#include <list>
+
+using namespace Mdt::Entity;
+
+void DataListTest::initTestCase()
+{
+}
+
+void DataListTest::cleanupTestCase()
+{
+}
+
+struct ArticleDataStruct
+{
+  qlonglong id;
+  QString description;
+};
+
+MDT_ENTITY_DEF(
+  (ArticleDataStruct),
+  Article,
+  (id, FieldFlag::IsRequired, FieldFlag::IsUnique),
+  (description, FieldMaxLength(250))
+)
+
+class ArticleData : public Mdt::Entity::DataTemplate<ArticleDataStruct, ArticleDef>
+{
+ public:
+
+  void setId(int id)
+  {
+    dataStruct().id = id;
+  }
+
+  int id() const
+  {
+    return constDataStruct().id;
+  }
+
+};
+
+template<typename Container>
+using ArticleDataList = DataList<ArticleData, Container>;
+
+/*
+ * Tests
+ */
+
+template<typename Container>
+void commonTestImpl()
+{
+  // Default constructor
+  ArticleDataList<Container> l0;
+  QCOMPARE(l0.size(), 0);
+  QCOMPARE(l0.count(), 0);
+  QVERIFY(l0.isEmpty());
+  
+  // Initial size with default constructed elements
+  ArticleDataList<Container> l1(1);
+  QCOMPARE(l1.size(), 1);
+  QCOMPARE(l1.count(), 1);
+  QVERIFY(!l1.isEmpty());
+  // Set value
+  l1[0].setId(11);
+  QCOMPARE(l1.at(0).id(), 11);
+  // Cleare
+  l1.clear();
+  QVERIFY(l1.isEmpty());
+  // Add / remove
+  ArticleData ad1;
+  ad1.setId(1);
+  l1.append(ad1);
+  QCOMPARE(l1.count(), 1);
+  QCOMPARE(l1.at(0).id(), 1);
+  l1.removeLast();
+  QVERIFY(l1.isEmpty());
+}
+
+void DataListTest::commonTest()
+{
+  commonTestImpl< std::vector<ArticleData> >();
+  commonTestImpl< std::list<ArticleData> >();
+  commonTestImpl< QVector<ArticleData> >();
+  commonTestImpl< QList<ArticleData> >();
+}
+
+/*
+ * Main
+ */
+
+int main(int argc, char **argv)
+{
+  Mdt::CoreApplication app(argc, argv);
+  DataListTest test;
+
+//   app.debugEnvironnement();
+
+  return QTest::qExec(&test, argc, argv);
+}
