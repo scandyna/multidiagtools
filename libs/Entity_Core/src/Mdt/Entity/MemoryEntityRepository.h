4343aeb990db37c65bd89420d09c02c5d7488d26
Philippe Steinmann <philippe.steinmann@gmail.com>
2018-08-15 09:53:27 +0200

    Entity: create MemoryEntityRepository

---
+++ libs/Entity_Core/src/Mdt/Entity/MemoryEntityRepository.h
@@ -0,0 +1,142 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2011-2018 Philippe Steinmann.
+ **
+ ** This file is part of Mdt library.
+ **
+ ** Mdt is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** Mdt is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with Mdt.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+#ifndef MDT_ENTITY_MEMORY_ENTITY_REPOSITORY_H
+#define MDT_ENTITY_MEMORY_ENTITY_REPOSITORY_H
+
+#include "AbstractEntityRepository.h"
+#include "Value.h"
+#include "Mdt/Error.h"
+#include <QMap>
+#include <QString>
+
+namespace Mdt{ namespace Entity{
+
+  /*! \brief Memory implementation for AbstractEntityRepository
+   *
+   * This repository implementation can be used for unit tests.
+   *
+   * The simplest way to avoid repeating template arguments
+   *  is to create a test class:
+   * \code
+   * class TestPersonRepository : public Mdt::Entity::MemoryEntityRepository<PersonData, PersonId>
+   * {
+   * };
+   * \endcode
+   *
+   * If a interafce repository is used in the business code,
+   *  the test class will look like tis:
+   * \code
+   * class TestPersonRepository : public PersonRepository, public Mdt::Entity::MemoryEntityRepository<PersonData, PersonId>
+   * {
+   * };
+   * \endcode
+   *
+   * The repository could be instanciated with a shared pointer:
+   * \code
+   * auto personRepository = std::make_shared<TestPersonRepository>();
+   * \endcode
+   */
+  template<typename EntityData, typename UniqueId>
+  class MemoryEntityRepository : public virtual AbstractEntityRepository<EntityData, UniqueId>
+  {
+    using ParentClass = AbstractEntityRepository<EntityData, UniqueId>;
+    using ParentClass::tr;
+
+   public:
+
+    /*! \brief Constructor
+     */
+    explicit MemoryEntityRepository(QObject *parent = nullptr)
+     : AbstractEntityRepository<EntityData, UniqueId>(parent)
+    {
+    }
+
+    /*! \brief Add \a record to the storage
+     *
+     * \note Currently only works for entities that have a single Id,
+     *   not a composed PK
+     */
+    Mdt::Expected<UniqueId> add(const EntityData & record) override
+    {
+      auto id = uniqueIdValue<EntityData, UniqueId>(record);
+
+      if(id.isNull()){
+        id = UniqueId(nextId());
+        auto recordCpy = record;
+        setUniqueIdValue<EntityData, UniqueId>(recordCpy, id);
+        mTable.insert( id.value(), recordCpy );
+      }else{
+        if(mTable.contains(id.value())){
+          const auto msg = tr("ljdjlkdfj");
+          auto error = mdtErrorNewQ(msg, Mdt::Error::Critical, this);
+          return error;
+        }
+        mTable.insert( id.value(), record );
+      }
+
+      return id;
+    }
+
+    /*! \brief Get a entity by its id
+     *
+     * If the record matching \a id was found, the record is returned.
+     *  If no record was found, a empty one is returned.
+     *  If a error occured, this error is returned.
+     *
+     * \pre \a id must not be null
+     */
+    Mdt::Expected<EntityData> getById(UniqueId id) const override
+    {
+      Q_ASSERT(!id.isNull());
+
+      return mTable.value(id.value());
+    }
+
+    /*! \brief Get a entity by its primary key
+     *
+     * If the record matching \a pk was found, the record is returned.
+     *  If no record was found, a empty one is returned.
+     *  If a error occured, this error is returned.
+     *
+     * \pre \a pk must not be null
+     */
+    Mdt::Expected<EntityData> getByPrimaryKey(const PrimaryKeyRecord & pk) const override
+    {
+      Q_ASSERT(!pk.isNull());
+    }
+
+   private:
+
+    int nextId() const
+    {
+      if(mTable.isEmpty()){
+        return 1;
+      }
+      return mTable.lastKey() + 1;
+    }
+
+    QMap<int, EntityData> mTable;
+
+  };
+
+}} // namespace Mdt{ namespace Entity{
+
+#endif // #ifndef MDT_ENTITY_MEMORY_ENTITY_REPOSITORY_H
---
+++ libs/Entity_Core/tests/src/MemoryEntityRepositoryTest.cpp
@@ -0,0 +1,245 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2011-2018 Philippe Steinmann.
+ **
+ ** This file is part of multiDiagTools library.
+ **
+ ** multiDiagTools is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** multiDiagTools is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with multiDiagTools.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+#include "MemoryEntityRepositoryTest.h"
+#include "Mdt/Entity/MemoryEntityRepository.h"
+#include "Mdt/Entity/Def.h"
+#include "Mdt/Entity/IntegralUniqueIdTemplate.h"
+#include "Mdt/Entity/DataTemplate.h"
+
+/*
+ * Entities
+ */
+
+struct PersonDataStruct
+{
+  int id = 0;
+  QString firstName;
+};
+
+MDT_ENTITY_DEF(
+  (PersonDataStruct),
+  Person,
+  (id, FieldFlag::IsPrimaryKey),
+  (firstName, FieldMaxLength(250))
+)
+
+class PersonId : public Mdt::Entity::IntegralUniqueIdTemplate<int>
+{
+ public:
+
+  using IntegralUniqueIdTemplate::IntegralUniqueIdTemplate;
+};
+
+class PersonData : public Mdt::Entity::DataTemplate<PersonEntity>
+{
+ public:
+
+  void setPersonId(PersonId id)
+  {
+    Q_ASSERT(!id.isNull());
+    dataStruct().id = id.value();
+  }
+
+  PersonId id() const
+  {
+    return PersonId(constDataStruct().id);
+  }
+
+  void setFirstName(const QString & name)
+  {
+    dataStruct().firstName = name;
+  }
+
+  QString firstName() const
+  {
+    return constDataStruct().firstName;
+  }
+};
+
+/*
+ * Repositories
+ */
+
+class GenericTestPersonRepository : public Mdt::Entity::MemoryEntityRepository<PersonData, PersonId>
+{
+};
+
+class PersonRepository : public virtual Mdt::Entity::AbstractEntityRepository<PersonData, PersonId>
+{
+};
+
+class TestPersonRepository : public PersonRepository, public Mdt::Entity::MemoryEntityRepository<PersonData, PersonId>
+{
+};
+
+using namespace Mdt::Entity;
+
+/*
+ * Tests
+ */
+
+void MemoryEntityRepositoryTest::genericUsageTest()
+{
+  auto repository = std::make_shared<GenericTestPersonRepository>();
+
+  PersonId personId(1);
+  PersonData personData;
+  auto personDataExp = repository->getById(personId);
+  QVERIFY(personDataExp);
+  personData = *personDataExp;
+  QVERIFY(personData.id().isNull());
+  QVERIFY(personData.firstName().isEmpty());
+
+  personData.setPersonId(personId);
+  personData.setFirstName("P1");
+  auto personIdExp = repository->add(personData);
+  QVERIFY(personIdExp);
+  personId = *personIdExp;
+  QCOMPARE(personId.value(), 1);
+
+  personDataExp = repository->getById(personId);
+  QVERIFY(personDataExp);
+  personData = *personDataExp;
+  QCOMPARE(personData.id().value(), 1);
+  QCOMPARE(personData.firstName(), QString("P1"));
+}
+
+void MemoryEntityRepositoryTest::entityImplClassTest()
+{
+  auto testRepository = std::make_shared<TestPersonRepository>();
+  // We have to check check polymorphic calls
+  std::shared_ptr<PersonRepository> repository = testRepository;
+
+  PersonId personId(1);
+  PersonData personData;
+  auto personDataExp = repository->getById(personId);
+  QVERIFY(personDataExp);
+  personData = *personDataExp;
+  QVERIFY(personData.id().isNull());
+  QVERIFY(personData.firstName().isEmpty());
+
+  personData.setPersonId(personId);
+  personData.setFirstName("P1");
+  auto personIdExp = repository->add(personData);
+  QVERIFY(personIdExp);
+  personId = *personIdExp;
+  QCOMPARE(personId.value(), 1);
+
+  personDataExp = repository->getById(personId);
+  QVERIFY(personDataExp);
+  personData = *personDataExp;
+  QCOMPARE(personData.id().value(), 1);
+  QCOMPARE(personData.firstName(), QString("P1"));
+}
+
+void MemoryEntityRepositoryTest::addWithNullIdTest()
+{
+  auto repository = std::make_shared<TestPersonRepository>();
+  PersonId personId;
+  PersonData person;
+
+  PersonData person1;
+  person1.setFirstName("P1");
+  QVERIFY(person1.id().isNull());
+  auto personIdExp = repository->add(person1);
+  QVERIFY(personIdExp);
+  personId = *personIdExp;
+  QCOMPARE(personId.value(), 1);
+  auto personDataExp = repository->getById(personId);
+  QVERIFY(personDataExp);
+  person = *personDataExp;
+  QCOMPARE(person.id().value(), 1);
+  QCOMPARE(person.firstName(), QString("P1"));
+
+  PersonData person2;
+  person2.setFirstName("P2");
+  QVERIFY(person2.id().isNull());
+  personIdExp = repository->add(person2);
+  QVERIFY(personIdExp);
+  personId = *personIdExp;
+  QCOMPARE(personId.value(), 2);
+  personDataExp = repository->getById(personId);
+  QVERIFY(personDataExp);
+  person = *personDataExp;
+  QCOMPARE(person.id().value(), 2);
+  QCOMPARE(person.firstName(), QString("P2"));
+}
+
+void MemoryEntityRepositoryTest::addWithIdTest()
+{
+  auto repository = std::make_shared<TestPersonRepository>();
+  PersonId personId;
+  PersonData person;
+
+  person.setPersonId(PersonId(10));
+  person.setFirstName("P10");
+  auto personIdExp = repository->add(person);
+  QVERIFY(personIdExp);
+  personId = *personIdExp;
+  QCOMPARE(personId.value(), 10);
+  auto personDataExp = repository->getById(personId);
+  QVERIFY(personDataExp);
+  person = *personDataExp;
+  QCOMPARE(person.id().value(), 10);
+  QCOMPARE(person.firstName(), QString("P10"));
+
+  person.setPersonId(PersonId(15));
+  person.setFirstName("P15");
+  personIdExp = repository->add(person);
+  QVERIFY(personIdExp);
+  personId = *personIdExp;
+  QCOMPARE(personId.value(), 15);
+  personDataExp = repository->getById(personId);
+  QVERIFY(personDataExp);
+  person = *personDataExp;
+  QCOMPARE(person.id().value(), 15);
+  QCOMPARE(person.firstName(), QString("P15"));
+}
+
+void MemoryEntityRepositoryTest::addIdUniqueConstraintTest()
+{
+  auto repository = std::make_shared<TestPersonRepository>();
+  PersonId personId;
+  PersonData person;
+
+  person.setPersonId(PersonId(5));
+  person.setFirstName("P5");
+  auto personIdExp = repository->add(person);
+  QVERIFY(personIdExp);
+  personId = *personIdExp;
+  QCOMPARE(personId.value(), 5);
+  personIdExp = repository->add(person);
+  QVERIFY(!personIdExp);
+}
+
+/*
+ * Main
+ */
+
+int main(int argc, char **argv)
+{
+  Mdt::CoreApplication app(argc, argv);
+  MemoryEntityRepositoryTest test;
+
+//   app.debugEnvironnement();
+
+  return QTest::qExec(&test, argc, argv);
+}
