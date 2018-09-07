2da8a3f5623ac545c2ca2bbe13e4d97e9e70591a
Philippe Steinmann <philippe.steinmann@gmail.com>
2018-09-07 08:38:11 +0200

    Railway: create ChooseVehicleTypeClassCache

---
+++ libs/Railway_Core/src/Mdt/Railway/ChooseVehicleTypeClassCache.cpp
@@ -0,0 +1,49 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2011-2018 Philippe Steinmann.
+ **
+ ** This file is part of Mdt library.
+ **
+ ** Mdt is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** Mdt is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with Mdt.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+#include "ChooseVehicleTypeClassCache.h"
+
+namespace Mdt{ namespace Railway{
+
+ChooseVehicleTypeClassCache::ChooseVehicleTypeClassCache(QObject* parent)
+ : AbstractAsyncReadOnlyCache(parent)
+{
+  mSelectStatement.addField( mSelectStatement.def().id() );
+  mSelectStatement.addField( mSelectStatement.def().name() );
+}
+
+void ChooseVehicleTypeClassCache::setQueryFactory(const std::shared_ptr< ChooseVehicleTypeClassCache::SelectQueryFactory >& factory)
+{
+  Q_ASSERT(factory.get() != nullptr);
+
+  mSelectQueryFactory = factory;
+  mSelectQuery = std::move( mSelectQueryFactory->createSelectQuery() );
+  connect( mSelectQuery.get(), &SelectQuery::newRecordAvailable, this, &ChooseVehicleTypeClassCache::fromBackendAppendRecord );
+  connect( mSelectQuery.get(), &SelectQuery::errorOccured, this, &ChooseVehicleTypeClassCache::setLastError );
+}
+
+bool ChooseVehicleTypeClassCache::fetchRecords(int count)
+{
+  mSelectQuery->submitStatement(mSelectStatement, count);
+
+  return true;
+}
+
+}} // namespace Mdt{ namespace Railway{
---
+++ libs/Railway_Core/src/Mdt/Railway/ChooseVehicleTypeClassCache.h
@@ -0,0 +1,78 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2011-2018 Philippe Steinmann.
+ **
+ ** This file is part of Mdt library.
+ **
+ ** Mdt is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** Mdt is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with Mdt.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+#ifndef MDT_RAILWAY_CHOOSE_VEHICLE_TYPE_CLASS_CACHE_H
+#define MDT_RAILWAY_CHOOSE_VEHICLE_TYPE_CLASS_CACHE_H
+
+#include "Entity/VehicleTypeClass.h"
+#include "Mdt/Entity/AbstractAsyncReadOnlyCache.h"
+#include "Mdt/Entity/EntitySelectStatement.h"
+#include "Mdt/QueryExpression/AbstractAsyncSelectQuery.h"
+#include "Mdt/QueryExpression/AbstractAsyncSelectQueryFactory.h"
+#include "MdtRailway_CoreExport.h"
+#include <memory>
+
+namespace Mdt{ namespace Railway{
+
+  /*! \brief
+   */
+  class MDT_RAILWAY_CORE_EXPORT ChooseVehicleTypeClassCache : public Mdt::Entity::AbstractAsyncReadOnlyCache
+  {
+   Q_OBJECT
+
+    using SelectQuery = Mdt::QueryExpression::AbstractAsyncSelectQuery;
+    using SelectQueryFactory = Mdt::QueryExpression::AbstractAsyncSelectQueryFactory;
+
+   public:
+
+    /*! \brief Constructor
+     */
+    ChooseVehicleTypeClassCache(QObject *parent = nullptr);
+
+    /*! \brief Set the query factory
+     */
+    void setQueryFactory(const std::shared_ptr<SelectQueryFactory> & factory);
+
+    /*! \brief Get count of columns
+     */
+    int columnCount() const noexcept override
+    {
+      return mSelectStatement.fieldCount();
+    }
+
+    /*! \brief Get the name column
+     */
+    int nameColumn() const noexcept
+    {
+      return 1;
+    }
+
+   private:
+
+    bool fetchRecords(int count) override;
+
+    Mdt::Entity::EntitySelectStatement<Entity::VehicleTypeClassEntity> mSelectStatement;
+    std::shared_ptr<SelectQueryFactory> mSelectQueryFactory;
+    std::unique_ptr<SelectQuery> mSelectQuery;
+  };
+
+}} // namespace Mdt{ namespace Railway{
+
+#endif // #ifndef MDT_RAILWAY_CHOOSE_VEHICLE_TYPE_CLASS_CACHE_H
