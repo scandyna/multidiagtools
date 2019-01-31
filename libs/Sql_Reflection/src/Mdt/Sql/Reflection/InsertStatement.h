841c5d3a5e4fca7c5cc35d26a0263f9e142ef6d7
Philippe Steinmann <philippe.steinmann@gmail.com>
2019-01-31 06:38:41 +0100

    Sql Reflection: +insertStatementFromReflected()

---
+++ libs/Sql_Reflection/src/Mdt/Sql/Reflection/InsertStatement.h
@@ -0,0 +1,122 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2011-2019 Philippe Steinmann.
+ **
+ ** This file is part of Mdt library.
+ **
+ ** Mdt is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** Mdt is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with Mdt.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+#ifndef MDT_SQL_REFLECTION_INSERT_STATEMENT_H
+#define MDT_SQL_REFLECTION_INSERT_STATEMENT_H
+
+#include "Mdt/Reflection/StructAlgorithm.h"
+#include "Mdt/Reflection/FieldAlgorithm.h"
+#include "Mdt/Reflection/PrimaryKeyAlgorithm.h"
+#include "Mdt/Reflection/TypeTraits/IsStructDef.h"
+#include "Mdt/Reflection/TypeTraits/IsStructDefAssociatedWithReflectedStruct.h"
+#include "Mdt/Reflection/TypeTraits/IsPrimaryKeyClass.h"
+#include "Mdt/Sql/InsertStatement.h"
+#include "Mdt/Sql/FieldName.h"
+#include <QLatin1String>
+
+namespace Mdt{ namespace Sql{ namespace Reflection{
+
+  namespace Impl{
+
+    /*! \internal
+     */
+    template<typename Pk, typename Field, typename Value>
+    bool mustAddValueToStatement(const Value & value)
+    {
+      if(Mdt::Reflection::isNullValuePartOfAutoIncrementIdPrimaryKey<Pk, Field>(value)){
+        return false;
+      }
+      return true;
+    }
+
+    /*! \internal
+     */
+    template<typename Statement, typename Pk>
+    struct AddValueToStatement
+    {
+      AddValueToStatement(Statement & statement)
+       : mStatement(statement)
+      {
+      }
+
+      template<typename FieldValuePair>
+      void operator()(const FieldValuePair & p) const
+      {
+        using Mdt::Sql::FieldName;
+
+        if( mustAddValueToStatement<Pk, typename FieldValuePair::first_type>(p.second) ){
+          mStatement.addValue( FieldName(QLatin1String(Mdt::Reflection::fieldName<typename FieldValuePair::first_type>())), p.second );
+        }
+      }
+
+     private:
+
+      Statement & mStatement;
+    };
+
+  } // namespace Impl{
+
+  /*! \brief Get a insert statement from a reflected struct
+   *
+   * Create a SQL insert statement:
+   * \code
+   * const auto statement = Mdt::Sql::Reflection::insertStatementFromReflected<PersonDef, PersonPrimaryKey>(person);
+   * \endcode
+   * This statement can be used with a SQL insert query:
+   * \code
+   * Mdt::Sql::InsertQuery query(dbConnection);
+   *
+   * if(!query.execStatement(statement)){
+   *   // Error handling
+   * }
+   * \endcode
+   *
+   * If \a PrimaryKey is a AutoIncrementIdPrimaryKey,
+   *  and the value associated to it in \a data is null,
+   *  it will not be added to the set of values in the statement
+   *  (This way, the DBMS will auto-generate a new id).
+   *
+   * \pre \a StructDef must be a struct definition associated with \a Struct
+   * \pre \a PrimaryKey must be primary key class for a reflected struct
+   *
+   * \sa Mdt::Sql::InsertStatement
+   * \sa Mdt::Sql::InsertQuery
+   */
+  template<typename StructDef, typename PrimaryKey, typename Struct>
+  InsertStatement insertStatementFromReflected(const Struct & data)
+  {
+    static_assert( Mdt::Reflection::TypeTraits::IsStructDefAssociatedWithReflectedStruct<StructDef, Struct>::value ,
+                   "StructDef must be a struct definition associated with \a Struct" );
+    static_assert( Mdt::Reflection::TypeTraits::IsPrimaryKeyClass<PrimaryKey>::value,
+                   "PrimaryKey must be a primary key class for a reflected struct" );
+
+    InsertStatement statement;
+    Impl::AddValueToStatement<InsertStatement, PrimaryKey> f(statement);
+
+    statement.setTableName( QLatin1String(Mdt::Reflection::nameFromStructDef<StructDef>()) );
+    Mdt::Reflection::forEachFieldValuePairInStruct(data, f);
+
+    return statement;
+  }
+
+}}} // namespace Mdt{ namespace Sql{ namespace Reflection{
+
+
+#endif // #ifndef MDT_SQL_REFLECTION_INSERT_STATEMENT_H
---
+++ libs/Sql_Reflection/tests/src/QueryStatementTest.cpp
@@ -0,0 +1,151 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2011-2019 Philippe Steinmann.
+ **
+ ** This file is part of multiDiagTools library.
+ **
+ ** multiDiagTools is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** multiDiagTools is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with multiDiagTools.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ ****************************************************************************/
+#include "QueryStatementTest.h"
+#include <Mdt/Expected.h>
+#include <Mdt/Sql/InsertQuery.h>
+#include <Mdt/Sql/Reflection/InsertStatement.h>
+#include "Mdt/Reflection/ReflectStruct.h"
+#include "Mdt/Reflection/AutoIncrementIdPrimaryKey.h"
+#include "Mdt/Reflection/PrimaryKey.h"
+#include <QString>
+#include <QLatin1String>
+#include <type_traits>
+
+using namespace Mdt::Reflection;
+// using Mdt::Sql::Schema::FieldType;
+
+/*
+ * Person struct
+ */
+
+struct PersonDataStruct
+{
+  int id;
+  QString firstName;
+  QString lastName;
+};
+
+MDT_REFLECT_STRUCT(
+  (PersonDataStruct),
+  Person,
+  (id),
+  (firstName, FieldFlag::IsRequired),
+  (lastName, FieldFlag::HasDefaultValue)
+)
+
+/*
+ * Entities
+ */
+
+struct TestCaseDataStruct
+{
+  qlonglong id = 0;
+  QString name;
+  double qty = 1.0;
+};
+
+MDT_REFLECT_STRUCT(
+  (TestCaseDataStruct),
+  TestCase,
+  (id),
+  (name, FieldFlag::IsRequired, FieldMaxLength(100)),
+  (qty, FieldFlag::HasDefaultValue)
+)
+
+struct MyStruct
+{
+  int i = 0;
+};
+Q_DECLARE_METATYPE(MyStruct)
+
+struct FieldTypeTestStruct
+{
+  int int_type;
+  QString QString_type;
+  MyStruct MyStruct_type;
+};
+
+MDT_REFLECT_STRUCT(
+  (FieldTypeTestStruct),
+  FieldTypeTest,
+  (int_type),
+  (QString_type),
+  (MyStruct_type)
+)
+
+/*
+ * Tests
+ */
+
+void QueryStatementTest::insertStatementFromReflectedTest()
+{
+  using AutoIncIdPk = AutoIncrementIdPrimaryKey<PersonDef::id>;
+  using Pk = PrimaryKey<PersonDef::id>;
+
+  PersonDataStruct person;
+
+  person.id = 0;
+  person.firstName = QString::fromLocal8Bit("fN");
+  person.lastName = QString::fromLocal8Bit("lN");
+  auto statement = Mdt::Sql::Reflection::insertStatementFromReflected<PersonDef, AutoIncIdPk>(person);
+  QCOMPARE(statement.tableName(), QLatin1String("Person"));
+  QCOMPARE(statement.toFieldNameList(), QStringList({QLatin1String("firstName"),QLatin1String("lastName")}));
+  QCOMPARE(statement.toValueList(), QVariantList({QLatin1String("fN"),QLatin1String("lN")}));
+
+  person.id = 1;
+  person.firstName = QString::fromLocal8Bit("fN");
+  person.lastName = QString::fromLocal8Bit("lN");
+  statement = Mdt::Sql::Reflection::insertStatementFromReflected<PersonDef, AutoIncIdPk>(person);
+  QCOMPARE(statement.tableName(), QLatin1String("Person"));
+  QCOMPARE(statement.toFieldNameList(), QStringList({QLatin1String("id"),QLatin1String("firstName"),QLatin1String("lastName")}));
+  QCOMPARE(statement.toValueList(), QVariantList({1,QLatin1String("fN"),QLatin1String("lN")}));
+
+  /// \todo Check if passing a null ID to a non auto increment PK is valid
+  person.id = 0;
+  person.firstName = QString::fromLocal8Bit("fN");
+  person.lastName = QString::fromLocal8Bit("lN");
+  statement = Mdt::Sql::Reflection::insertStatementFromReflected<PersonDef, Pk>(person);
+  QCOMPARE(statement.tableName(), QLatin1String("Person"));
+  QCOMPARE(statement.toFieldNameList(), QStringList({QLatin1String("id"),QLatin1String("firstName"),QLatin1String("lastName")}));
+  QCOMPARE(statement.toValueList(), QVariantList({0,QLatin1String("fN"),QLatin1String("lN")}));
+
+  person.id = 1;
+  person.firstName = QString::fromLocal8Bit("fN");
+  person.lastName = QString::fromLocal8Bit("lN");
+  statement = Mdt::Sql::Reflection::insertStatementFromReflected<PersonDef, Pk>(person);
+  QCOMPARE(statement.tableName(), QLatin1String("Person"));
+  QCOMPARE(statement.toFieldNameList(), QStringList({QLatin1String("id"),QLatin1String("firstName"),QLatin1String("lastName")}));
+  QCOMPARE(statement.toValueList(), QVariantList({1,QLatin1String("fN"),QLatin1String("lN")}));
+}
+
+/*
+ * Main
+ */
+
+int main(int argc, char **argv)
+{
+  Mdt::CoreApplication app(argc, argv);
+  QueryStatementTest test;
+
+//   app.debugEnvironnement();
+
+  return QTest::qExec(&test, argc, argv);
+}
